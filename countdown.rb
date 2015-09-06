f652c0b4ddc27209b1abe4c8860defcc924ada94
eenymini <hhl262@nyu.edu>
2015-09-06 19:55:54 -0400

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+  
+  while countdown_output > 0
+    countdown_output -= 1
+    sleep 1
+  puts "#{countdown_output + 1} SECOND(S)!"
+  if countdown_output == 0
+    return "HAPPY NEW YEAR!"
+end
+end
+end
+
+def countdown_with_sleep(countdown_output)
+  
+  while countdown_output > 0
+    countdown_output -= 1
+    sleep 1
+  puts "#{countdown_output + 1} SECOND(S)!"
+  if countdown_output == 0
+    return "HAPPY NEW YEAR!"
+end
+end
 end
